cd3c19ffd633ffe9a1dcd9c2dbbb874b19e44273
YAGO MURILO BERNARDES SILVA <25162468@edu119.local>
2025-08-29 10:00:51 -0300

    adicionando PUT

--- server.js
+++ server.js
@@ -1,38 +1,25 @@
-const express = require("express")
-
+// importa a biblioteca
+const express = require("express"); // framework web
+// cria a aplicação express
 const app = express();
-
-app.use(express.json())
-
+app.use(express.json());
 const PORT = 3000;
 
 const ALUNOS = [
-
-]
-
-const alunos =[
     {
-        id: 1,
-        nome: "vitor lindao",
-        cor: "amarelo",
-        idade: "18"
+        id: 1, nome: "Vitor Lima", cor: "Azul", idade: 26
     },
     {
-        id: 2,
-        nome: "neymar",
-        cor: "azul",
-        idade: "33"
+        id: 2, nome: "João Gamer", cor: "Preto",  idade: 17
     },
     {
-        id: 3,
-        nome: "foden",
-        cor: "branco",
-        idade: "27"
-    }
+        id: 3, nome: "Henry Sem ducha",cor: "Verde",idade: 19
+    },
 ]
-app.get("/",(req, res)=>{
+
+app.get("/", (req, res) => {
     res.json({
-    msg: "hello world"
+        mensagem: "Hello world"
     })
 })
 
@@ -40,20 +27,83 @@ app.get("/alunos",(req, res)=>{
     res.json(ALUNOS);
 })
 
-app.get("/alunos/:id", (req, res)=>{ 
+app.get("/alunos/:id", (req , res)=>{
     const id = Number(req.params.id)
-    console.log(`valor recebido ${id}`);
-    const aluno = ALUNOS.filter( (a) => a.nome == id )
-    if(aluno.length > 1){
-        res.status(200).json(aluno) 
-    }
-    res.status(404).json({msg: "alunos nao encontrado"}) 
-})
-
-app.get("/alunos/:cor", (req, res)=>{
+    console.log(`Valor recebido ${id}`);
     
+    const aluno = ALUNOS.filter( (aluno) => aluno.id === id )
+    if(aluno.length > 0){
+        res.status(200).json(aluno)
+    }else{
+        res.status(404).json({ msg: "Aluno não encontrado"})
+    }     
 })
 
-app.listen(PORT, ()=>{
-    console.log(`Servidor rodadando em http//:localhost:${PORT}`)
+app.get("/alunos/cor/:cor", (req, res) => {
+    const cor = req.params.cor;
+    console.log(`Cor recebida: ${cor}`);    
+    const alunosFiltrados = ALUNOS.filter(
+        (aluno) => aluno.cor.toLowerCase() === cor.toLowerCase()
+    );
+    if (alunosFiltrados.length > 0) {
+        res.status(200).json(alunosFiltrados);
+    } else {
+        res.status(404).json({ msg: "Nenhum aluno encontrado com essa cor" });
+    }
+});
+
+app.post("/alunos", (req, res)=>{
+    const{nome, cor, idade}  = req.body;
+
+    if(nome || !cor || !idade)
+        return res.status(400).json({msg : "nome cor e idade sao obrigatoria"})
+})
+
+app.delete("/alunos/:id", (req, res)=>{
+    const id = Number(req.params.id);
+    const indice = ALUNOS.findIndex((aluno)=>{ aluno.id === id})
+    console.log(indice)
+})
+
+
+app.post("/alunos", (req, res)=>{
+    const informaçao = req.body;
+    console.log(informaçao);
+    ALUNOS.push(informaçao);
+    res.status(201).json({mensagem: "aluno criado com sucesso"});
+})
+
+
+app.put("/alunos/:id", (req, res) =>{
+    const id = Number(req.params.id);
+    const {nome, cor, idade} = req.body;
+
+    const indice = ALUNOS.findIndex(aluno => aluno.id === id)
+
+    if(indice === -1){
+        return res.status(404),json({msg: "aluno nao encontrado"})
+    }
+    if(!nome || !cor || !idade){
+        return res.status(400).json({msg: "nome, cor e idade sao obrigatorias"})
+    }
+    ALUNOS[indice] = {id, nome, cor, idade}
+
+res.status(200).json({msg: "aluno atualizado com sucesso!"})
+
+    // console.log(indice)
+    // console.log(ALUNOS[indice])
+
+
+})
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 })
